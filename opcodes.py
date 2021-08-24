69f85305d47731ce4ff5b7d2d83ce619744f4253
Andrija Radica <andrija.radica@gmail.com>
2021-08-24 09:54:26 +0200

    Add more instructions

--- opcodes.py
+++ opcodes.py
@@ -1,7 +1,11 @@
 # I/O: 100
 LOAD = 100
 PRINT = 101
+LOADP = 102
 INPUT = 110
+INPUTP = 111
+COPY = 120
+
 
 # Arithmetic
 INC = 200
@@ -18,12 +22,16 @@ GOTO = 311
 
 # Halt
 HALT = 400
-
+META = 401
+NOP = 402
 
 OPCODES = {
     "LOAD": LOAD,
     "PRINT": PRINT,
+    "LOADP": LOADP,
     "INPUT": INPUT,
+    "INPUTP": INPUTP,
+    "COPY": COPY,
     "INC": INC,
     "ADD": ADD,
     "SUB": SUB,
@@ -33,14 +41,19 @@ OPCODES = {
     "GOL": GOL,
     "GOE": GOE,
     "GOTO": GOTO,
-    "HALT": HALT
+    "HALT": HALT,
+    "META": META,
+    "NOP": NOP
 }
 
 
 ARGS = {
     LOAD: 2,
     PRINT: 1,
+    LOADP: 2,
     INPUT: 1,
+    INPUTP: 1,
+    COPY: 2,
     INC: 1,
     ADD: 3,
     SUB: 3,
@@ -50,5 +63,7 @@ ARGS = {
     GOL: 3,
     GOE: 3,
     GOTO: 1,
-    HALT: 0
+    HALT: 0,
+    META: 2,
+    NOP: 0
 }
