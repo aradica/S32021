c69aecad5668d9b5ed4950b10104391630211545
Frane Livić <83946219+Frane-Livic@users.noreply.github.com>
2021-08-25 03:03:23 +0200

    Refactored code into new class

--- fhvm.py
+++ fhvm.py
@@ -6,6 +6,7 @@ Project: Turing Complete
 Višnjan, 2021
 """
 from opcodes import *
+from preprocessor import Preprocessor
 import argparse
 
 
@@ -21,7 +22,7 @@ class VirtualMachine:
         self.p_registers = p_registers
         # Pokazivač na programski registar
         self.i = 0
-        self.vars = {}
+
 
     def __repr__(self):
         return f"[VM] R: {self.registers}\nP:{self.program}"
@@ -95,94 +96,10 @@ class VirtualMachine:
         f.loadProgram(newProgram)
 
     ###################
-
     def loadProgramFile(self, filename):
-        """Loads raw program from file and returns list of lists(lines)"""
-        with open(filename, "r") as file:
-            lines = file.readlines()
-            lines = [line.split(";")[0].split() for line in lines]
-        # print(lines)
-        # return lines
-        if DEBUG:
-            print("[DEBUG]", lines)
-        program = self.preprocess(lines)
+        preprocessor = Preprocessor()
+        program, newProgram, startR, endR = preprocessor.process(filename)
         self.loadProgram(program)
-
-    def preprocessDefs(self, lines):
-        defs = []
-        enddefs = []
-        for i, line in enumerate(lines):
-            if line[0] == "DEF":
-                defs.append(i)
-            elif line[0] == "ENDDEF":
-                enddefs.append(i)
-
-        return [(d, e) for d, e in zip(defs, reversed(enddefs))]
-
-    def copyDef(self, endpoints):
-        newProgram =  self.p_registers
-        newProgram.append()
-
-    def preprocess(self, lines):
-        """
-        Replaces codes with numeric opcodes and
-        replaces line numbers with actual indexes
-        """
-        program = []
-        lineSet = set()
-        i = 0
-        for line in lines:
-            cmd = line[0]
-            lineSet.add(i)
-            program.append(OPCODES[cmd])
-            if(line[0] == "LIST"):
-                for h in range(int(line[2])):
-                    self.vars[line[1] + str(h)] = len(self.vars)
-                continue
-            for arg in line[1:]:
-                if arg < 'A':
-                    program.append(int(arg))
-                elif("[" in arg):
-                    var = ""
-                    for h in arg:
-                        name = ""
-                        for k in arg:
-                            if(k == "["):
-                                break
-                            name += k
-                        if(h in [str(g) for g in range(10)]):
-                            var += h
-                    program.append(self.vars[name+var])
-                elif (arg >= 'A' and arg <= 'Z' or arg >= 'a' and arg <= 'z'):
-                    if arg not in self.vars:
-                        self.vars[arg] = len(self.vars)
-                        program.append(self.vars[arg])
-                    else:
-                        program.append(self.vars[arg])
-                else:
-                    print("Error: Incorrect Input Value")
-                i += 1
-            i += 1
-        i = 0
-        for line in lines:
-            cmd = line[0]
-            if OPCODES[cmd] in [GOG, GOL, GOE, GOTO]:
-                proSet = sorted(list(lineSet))
-                #print("debug:", proSet, program, line)
-                program[i+1] = proSet[int(line[1])-1]
-            i += ARGS[OPCODES[cmd]]
-            i += 1
-        i = 0
-        # for line in lines:
-        #     cmd = line[0]
-            
-        #     if OPCODES[cmd] in [DEF]: #TODO dodaj DEF u OPCODES
-        #         endpoints = program.preprocessDefs()
-        #         newProgram = program.copyDef(endpoints)
-        #     i += 1
-        if DEBUG:
-            print("[DEBUG]", program)
-        return program
 
     def loadProgram(self, program):
         if len(program) > self.p_registers:
